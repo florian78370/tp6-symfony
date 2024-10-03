f1ac621f44e52c8ed380366b834f0d31359c255b
Florian FORGET <florian.forget@btssio.dedyn.io>
2024-10-03 12:04:21 +0200

    vidéo 14 7min17

--- src/Controller/Admin/ArtisteController.php
+++ src/Controller/Admin/ArtisteController.php
@@ -2,25 +2,37 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Artiste;
+use App\Form\ArtisteType;
+use App\Repository\ArtisteRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArtisteController extends AbstractController
 {
     
-        #[Route('/admin/artistes', name: 'admin_artist es', methods :'GET')]
-        public function listeArtistes(ArtisteRepository $repo) : Response
+        #[Route('/admin/artistes', name: 'admin_artistes', methods :'GET')]
+        public function listeArtistes(ArtisteRepository $repo, PaginatorInterface $paginator, Request $request) : Response
         {
             $artistes=$paginator->paginate(
-                $repo->listeArtistesComplete(),
+                $repo->listeArtistesCompletePaginee(),
                 $request->query->getInt('page', 1), /*page number*/
                 8 /*limit per page*/
             );
-            return $this->render('artiste/listeArtistes.html.twig', [
+            return $this->render('admin/artiste/listeArtistes.html.twig', [
                 'lesArtistes' => $artistes
             ]);
-
-          
         }
+
+        #[Route('/admin/artistes/ajout', name: 'admin_artiste_ajout', methods :'GET, POST')]
+        public function AjouterAtiste() : Response
+        {
+            $artiste=new Artiste();
+            $form=$this->createForm(ArtisteType::class, $artiste);
+            return $this->render('admin/artiste/listeArtistes.html.twig', [
+                'lesArtistes' => $artistes
+            ]);
 }
--- src/Repository/ArtisteRepository.php
+++ src/Repository/ArtisteRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Artiste;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Artiste>
@@ -35,6 +36,19 @@ class ArtisteRepository extends ServiceEntityRepository
        ;
     }
 
+
+     /**
+    * @return Query Returns an array of Artiste objects
+    */
+   public function listeArtistesCompletePaginee(): Query
+   {
+      return $this->createQueryBuilder('art')
+                ->select('art','a')
+                ->innerJoin('art.albums', 'a')
+                ->orderBy('art.nom', 'ASC')
+           ->getQuery()
+       ;
+    }
 //    public function findOneBySomeField($value): ?Artiste
 //    {
 //        return $this->createQueryBuilder('a')
