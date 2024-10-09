5a9ee07cb110e6b9485208f2442c12af4a417654
Florian FORGET <florian.forget@btssio.dedyn.io>
2024-10-09 17:09:32 +0200

    vidéo 17 fin

--- src/Controller/Admin/ArtisteController.php
+++ src/Controller/Admin/ArtisteController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Artiste;
 use App\Form\ArtisteType;
 use App\Repository\ArtisteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,11 +30,37 @@ class ArtisteController extends AbstractController
 
         #[Route('/admin/artiste/ajout', name: 'admin_artiste_ajout', methods : ["GET", "POST"])]
         
-        public function AjouterAtiste() : Response
+        public function AjouterArtiste( Request $request, EntityManagerInterface $manager) : Response
         {
             $artiste=new Artiste();
             $form=$this->createForm(ArtisteType::class, $artiste);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) { 
+                $manager->persist($artiste);
+                $manager->flush();
+                $this->addFlash("success","L'artiste a bien été ajouté");
+                return $this->redirectToRoute('admin_artistes');
+            }
             return $this->render('admin/artiste/formAjoutArtiste.html.twig', [
+                'formArtiste' => $form->createView()
+            ]);
+        }
+
+        #[Route('/admin/artiste/modif/{id}', name: 'admin_artiste_modif', methods : ["GET", "POST"])]
+        
+        public function modifArtiste(Artiste $artiste, Request $request, EntityManagerInterface $manager) : Response
+        {
+            $form=$this->createForm(ArtisteType::class, $artiste);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) 
+            { 
+                $manager->persist($artiste);
+                $manager->flush();
+                $this->addFlash("success","L'artiste a bien été modifié");
+                return $this->redirectToRoute('admin_artistes');
+            }
+            return $this->render('admin/artiste/formModifArtiste.html.twig', [
                 'formArtiste' => $form->createView()
             ]);
         }
--- src/Form/ArtisteType.php
+++ src/Form/ArtisteType.php
@@ -6,24 +6,45 @@ use App\Entity\Artiste;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArtisteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
-            ->add('site')
-            ->add('image')
-            ->add('type')
+            ->add('nom', TextType::class, [
+                'label'=>"nom de l'artiste",
+                "attr"=> [
+                'Placeholder'=>"saisir le nom de l'artiste"
+                ]
+            ])
+            ->add('description', TextareaType::class)
+            ->add('site', UrlType::class)
+            ->add('image', TextType::class)
+            ->add('type', ChoiceType::class,[
+                "choices"=>[
+                    "solo"=>0,
+                    "groupe"=>1
+                ]
+            ]) 
+            
             ->add('valider', SubmitType::class,[
                 "attr"=> [
-                    'class'=>"btn btn-success"
+                    'class'=>"btn btn-success btn-block"
                     ]
                 ])
-        ;
+            ->add('annuler', SubmitType::class,[
+                "attr"=> [
+                    'class'=>"btn btn-warning btn-block"
+                        ]
+                ])
+            
+        ;       
     }
 
     public function configureOptions(OptionsResolver $resolver): void
